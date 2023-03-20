d3214ca9c2192cd274aacbd18985b2bb55beaa9e
chris <ngmi@posteo.es>
2023-03-20 10:55:31 +0100

    updates

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 function QueryBalance() {
-  const [tokenID, setTokenID] = useState("");
+  const [tokenID, setTokenID] = useState("169"); // Set default tokenID value to 169
   const [balanceInfo, setBalanceInfo] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
@@ -23,6 +23,10 @@ function QueryBalance() {
       })
       .catch((error) => console.error(error));
   };
+
+  useEffect(() => {
+    queryBalance(); // Call queryBalance function on initial load with default tokenID value
+  }, []); // Empty dependency array ensures the function is called only once on initial load
 
   return (
     <div className="flex items-center justify-center">
